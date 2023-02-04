ec8e3b1c3dd07dd3cb298f4e7bef263d16e4afb8
신충호 <skfl78888@gmail.com>
2023-02-04 13:21:01 +0900

    20230204_1320T_SCH

--- BackTesting_System/backtesting.py
+++ BackTesting_System/backtesting.py
@@ -14,6 +14,17 @@ class BackTesting():
         '''년도별수익'''
         pass
     
+    def calc_mdd(self, start, end, code):
+        '''MDD 계산을 실시합니다.
+        MDD 계산 수식: (조회기간 내 최고가격 - 조회기간 내 최저가격) / 조회기간 내 최고가격 * 100.0
+        **args
+        -start:
+        -end:
+        -code:
+        **return
+        -'''
+        pass
+    
     def calc_sharp_index(self):
         '''샤프지수 계산'''
         pass
