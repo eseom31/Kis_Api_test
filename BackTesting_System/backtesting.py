ba2e510fd2ca6175ec55b5394b4c9864c8af480c
신충호 <skfl78888@gmail.com>
2023-02-16 23:03:14 +0900

    20230216_2303T_SCH

--- BackTesting_System/backtesting.py
+++ BackTesting_System/backtesting.py
@@ -1,3 +1,4 @@
+from typing import List
 import pandas as pd
 from copy import deepcopy
 
@@ -47,42 +48,103 @@ class EconomicIndicators:
     
     
     
-class BackTesting:
+class logSystem:
     def __init__(self):
-        self.portfolio = pd.DataFrame(columns=['날짜','매수총합','매도총합','매매비용','실현손익'])
-        self.jounal = pd.DataFrame(columns=['날짜', '종목코드', '체결단가', '체결수량', '매매비용', '매매구분'])
-        self.asset = 0
-        
-    def buy(self,
-            handle_data: 'pd.DataFrame'):
-        '''handle_data를 활용해 BackTesting 클래스 내 존재하는 jounal 속성에 일지를 작성합니다'''
-        hd = deepcopy(handle_data)
-        res = hd[hd['condition'] == True]
-        self.jounal['날짜'] = res['날짜']
-        self.jounal['종목코드'] = res['코드']
-        self.jounal['체결단가'] = res['종가']
-        self.jounal['체결수량'] = 1
-        self.jounal['매매비용'] = 0
-        self.jounal['매매구분'] = '매수'
-        
+        self.ECONO_INFORM = pd.DataFrame(columns=['날짜','매수총합','매도총합','매매비용','실현손익'])
+        self.JOUNAL = pd.DataFrame(columns=['날짜', '종목코드', '체결단가', '체결수량', '매매비용', '매매구분'])
 
-    def sell(self):
-        pass
+    def write_jounal(self, 
+                    inform: 'List[pd.DatetimeIndex, str, int, int, int, str]'):
+        '''BackTesting 클래스 내 존재하는 JOUNAL 속성에 일지를 작성합니다'''
+        buf = {col: val for col, val in zip(self.JOUNAL.columns, inform)}
+        self.JOUNAL = pd.concat([self.JOUNAL, pd.Series(buf).to_frame().T], ignore_index=True)
     
-    def update_portfolio(self):
-        for date in self.jounal['날짜'].unique():
-            contents = self.jounal[self.jounal['날짜'] == date]
+    def update_information(self):
+        '''Jounal Data 전체를 이용하여 portfolio를 갱신합니다'''
+        for date in self.JOUNAL['날짜'].unique():
+            contents = self.JOUNAL[self.JOUNAL['날짜'] == date]
+            tot_buy = contents[contents['매매구분']=='매수'][['체결단가', '체결수량']].apply(lambda x: x.prod(), axis=1).sum()
+            tot_sell = contents[contents['매매구분']=='매도'][['체결단가', '체결수량']].apply(lambda x: x.prod(), axis=1).sum()
+            deviation = tot_sell - tot_buy
             buf = pd.Series({
                 '날짜': date,
-                '매수총합': contents[contents['매매구분']=='매수']['체결단가'].sum(),
-                '매도총합': contents[contents['매매구분']=='매도']['체결단가'].sum(),
+                '매수총합': tot_buy,
+                '매도총합': tot_sell,
                 '매매비용': contents['매매비용'].sum(),
-                '실현손익': contents[contents['매매구분']=='매수']['체결단가'].sum() - contents[contents['매매구분']=='매도']['체결단가'].sum()})
-            self.portfolio = pd.concat([self.portfolio, buf.to_frame().T], ignore_index=True)
+                '실현손익': deviation if deviation > 0 else 0})
+            self.ECONO_INFORM = pd.concat([self.ECONO_INFORM, buf.to_frame().T], ignore_index=True)
+
+class BackTesting(logSystem):
+    def __init__(self):
+        super().__init__()
+        self.ASSET = {}
+        self.MONEY = 0
     
+    def upsert_asset(self,
+                    code,
+                    amount,
+                    sales_class):
+        if not code in self.ASSET.keys():  
+            self.ASSET[code] = amount
+        elif sales_class == '매수':
+            self.ASSET[code] += amount
+        else:
+            self.ASSET[code] -= amount
+        self.ASSET = {key: val for key, val in self.ASSET.items() if val != 0}
+        ##만약 남은 개수 0이면 목록에서 삭제프로그램 추가해야함
+            
+    def buy(self,
+            df: 'pd.DataFrame',
+            target: 'str'):
+        '''asset 관리
+        money 갱신만 실시
+        위탁수수료 0.0140527%로 계산 ##선민이형 확인 필요'''
+        hd = deepcopy(df)
+        tot_price = 0
+        for (_, trg), group_df in hd.groupby(['날짜', target], sort=True):
+            if trg == True:
+                for _, data in group_df.iterrows():
+                    tot_price += data['종가'] * data['구매수량']
+                if tot_price < self.MONEY:        
+                    for _, data in group_df.iterrows():
+                        inform = [data['날짜'],
+                                    data['코드'],
+                                    data['종가'],
+                                    data['구매수량'],
+                                    round(data['종가'] * data['구매수량'] * 0.00140527, 0),  
+                                    '매수']
+                        self.write_jounal(inform=inform)    
+                        self.MONEY -= data['종가'] * data['구매수량']
+                        self.upsert_asset(code=data['코드'], amount=data['구매수량'], sales_class='매수')
+                        
+    
+    def sell(self,
+            df: 'pd.DataFrame',
+            target: 'str'):
+        '''asset 관리
+        money 갱신만 실시
+        위탁수수료 0.0140527%로 계산 ##선민이형 확인 필요'''
+        hd = deepcopy(df)
+        for (_, trg), group_df in hd.groupby(['날짜', target], sort=True):
+            if trg == True:                
+                for _, data in group_df.iterrows():
+                    inform = [data['날짜'],
+                                data['코드'],
+                                data['종가'],
+                                self.ASSET[data['코드']],
+                                round(data['종가'] * self.ASSET[data['코드']] * 0.00140527, 0),  
+                                '매도']
+                    self.write_jounal(inform=inform)    
+                    self.MONEY += data['종가'] * self.ASSET[data['코드']]
+                    self.upsert_asset(code=data['코드'], amount=self.ASSET[data['코드']], sales_class='매도')
+                    
+            
     
 class Utls():
     def __init__(self):
+        pass
+    
+    def sort_df_by_day(self):
         pass
     
     def plot(self):
