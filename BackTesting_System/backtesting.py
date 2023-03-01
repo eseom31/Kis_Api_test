516ba6fbc104cf10610e7b9b6f598d630936eb18
신충호 <skfl78888@gmail.com>
2023-03-01 23:27:15 +0900

    20230301_2327T_SCH

--- BackTesting_System/backtesting.py
+++ BackTesting_System/backtesting.py
@@ -55,7 +55,22 @@ class logSystem:
     def __init__(self):
         self.ECONO_INFORM = pd.DataFrame(columns=['날짜','매수총합','매도총합','매매비용','실현손익'])
         self.JOUNAL = pd.DataFrame(columns=['날짜', '종목코드', '체결단가', '체결수량', '매매비용', '매매구분'])
-
+        self.ASSET_INFM = {}
+        
+    def upsert_asset_infm(self,
+                    code,
+                    amount,
+                    sales_class,
+                    price=None):
+        if not code in self.ASSET_INFM.keys():  
+            self.ASSET_INFM[code] = {'평단가': price, '수량': amount}
+        elif sales_class == '매수':
+            self.ASSET_INFM[code]['평단가'] = ((self.ASSET_INFM[code]['평단가'] * self.ASSET_INFM[code]['수량']) + (price * amount)) / (self.ASSET_INFM[code]['수량'] + amount)
+            self.ASSET_INFM[code]['수량'] += amount
+        else:
+            self.ASSET_INFM[code]['수량'] -= amount
+        self.ASSET_INFM = {key: val for key, val in self.ASSET_INFM.items() if val['수량'] != 0}
+    
     def write_jounal(self, 
                     inform: 'List[pd.DatetimeIndex, str, int, int, int, str]'):
         '''BackTesting 클래스 내 존재하는 JOUNAL 속성에 일지를 작성합니다'''
@@ -67,35 +82,42 @@ class logSystem:
         for date in self.JOUNAL['날짜'].unique():
             contents = self.JOUNAL[self.JOUNAL['날짜'] == date]
             tot_buy = contents[contents['매매구분']=='매수'][['체결단가', '체결수량']].apply(lambda x: x.prod(), axis=1).sum()
+            print(date)
+            # print(self.ASSET_INFM)
             tot_sell = contents[contents['매매구분']=='매도'][['체결단가', '체결수량']].apply(lambda x: x.prod(), axis=1).sum()
-            deviation = tot_sell - tot_buy
+            deviation = contents[contents['매매구분']=='매도'].apply(lambda x: (x['체결단가'] * x['체결수량']) - (self.ASSET_INFM[x['종목코드']]['평단가'] * self.ASSET_INFM[x['종목코드']]['수량']), axis=1).sum()  
+            real_profit = deviation if '매도' in list(contents['매매구분']) else 0.0
             buf = pd.Series({
                 '날짜': date,
                 '매수총합': tot_buy,
                 '매도총합': tot_sell,
                 '매매비용': contents['매매비용'].sum(),
-                '실현손익': tot_sell - contents['매매비용'].sum() - tot_buy}) #deviation if deviation > 0 else 0})
+                '실현손익': real_profit}) #deviation if deviation > 0 else 0})
             self.ECONO_INFORM = pd.concat([self.ECONO_INFORM, buf.to_frame().T], ignore_index=True)
-
+            for sales_class in ['매도', '매수']:    
+                for _, row in contents[contents['매매구분']==sales_class].iterrows():
+                    self.upsert_asset_infm(code=row['종목코드'], amount=row['체결수량'], sales_class=sales_class, price=row['체결단가'])
+                    
 class BackTesting(logSystem):
     def __init__(self):
         super().__init__()
-        self.ASSET = {}
         self.MONEY = 0
-    
+        self.ASSET = {}
+        
     def upsert_asset(self,
-                    code,
-                    amount,
-                    sales_class):
+                code,
+                amount,
+                sales_class,
+                price=None):
         if not code in self.ASSET.keys():  
-            self.ASSET[code] = amount
+            self.ASSET[code] = {'평단가': price, '수량': amount}
         elif sales_class == '매수':
-            self.ASSET[code] += amount
+            self.ASSET[code]['평단가'] = (self.ASSET[code]['평단가'] * self.ASSET[code]['수량']) + (price * amount) / (self.ASSET[code]['수량'] + amount)
+            self.ASSET[code]['수량'] += amount
         else:
-            self.ASSET[code] -= amount
-        self.ASSET = {key: val for key, val in self.ASSET.items() if val != 0}
-        ##만약 남은 개수 0이면 목록에서 삭제프로그램 추가해야함
-            
+            self.ASSET[code]['수량'] -= amount
+        self.ASSET = {key: val for key, val in self.ASSET.items() if val['수량'] != 0}
+
     def buy(self,
             df: 'pd.DataFrame',
             target: 'str'):
@@ -121,7 +143,7 @@ class BackTesting(logSystem):
                         self.MONEY -= data['종가'] * data['구매수량'] + round(data['종가'] * data['구매수량'] * 0.00140527, 0)
                         self.upsert_asset(code=str(data['코드']).zfill(6), amount=data['구매수량'], sales_class='매수')
                         
-    
+
     def sell(self,
             df: 'pd.DataFrame',
             target: 'str'):
@@ -146,9 +168,22 @@ class BackTesting(logSystem):
                 df: 'pd.DataFrame',
                 buy: 'str',
                 sell: 'str'):
+        self.test = {}
         hd = deepcopy(df)        
-        for (_, buy, sell), group_df in hd.groupby(['날짜', buy, sell], sort=True):
+        for (day, buy, sell), group_df in hd.groupby(['날짜', buy, sell], sort=True):
             try:
+                if sell == True:                
+                    for _, data in group_df.iterrows():
+                        inform = [data['날짜'],
+                                str(data['코드']).zfill(6),
+                                data['종가'],
+                                self.ASSET[str(data['코드']).zfill(6)]['수량'],
+                                round(data['종가'] * self.ASSET[str(data['코드']).zfill(6)]['수량'] * 0.00140527, 0),  
+                                '매도']
+                        self.write_jounal(inform=inform)    
+                        self.MONEY += data['종가'] * self.ASSET[str(data['코드']).zfill(6)]['수량'] - round(data['종가'] * self.ASSET[str(data['코드']).zfill(6)]['수량'] * 0.00140527, 0)
+                        self.upsert_asset(code=str(data['코드']).zfill(6), amount=self.ASSET[str(data['코드']).zfill(6)]['수량'], sales_class='매도')     
+                        self.test[day] = deepcopy(self.ASSET)
                 if buy == True:
                     tot_price = 0
                     for _, data in group_df.iterrows():
@@ -156,27 +191,18 @@ class BackTesting(logSystem):
                     if tot_price < self.MONEY:        
                         for _, data in group_df.iterrows():
                             inform = [data['날짜'],
-                                        str(data['코드']).zfill(6),
-                                        data['종가'],
-                                        data['구매수량'],
-                                        round(data['종가'] * data['구매수량'] * 0.00140527, 0),  
-                                        '매수']
-                            self.write_jounal(inform=inform)    
-                            self.MONEY -= data['종가'] * data['구매수량'] + round(data['종가'] * data['구매수량'] * 0.00140527, 0)
-                            self.upsert_asset(code=str(data['코드']).zfill(6), amount=data['구매수량'], sales_class='매수')         
-                if sell == True:                
-                    for _, data in group_df.iterrows():
-                        inform = [data['날짜'],
                                     str(data['코드']).zfill(6),
                                     data['종가'],
-                                    self.ASSET[str(data['코드']).zfill(6)],
-                                    round(data['종가'] * self.ASSET[str(data['코드']).zfill(6)] * 0.00140527, 0),  
-                                    '매도']
-                        self.write_jounal(inform=inform)    
-                        self.MONEY += data['종가'] * self.ASSET[str(data['코드']).zfill(6)] - round(data['종가'] * self.ASSET[str(data['코드']).zfill(6)] * 0.00140527, 0)
-                        self.upsert_asset(code=str(data['코드']).zfill(6), amount=self.ASSET[str(data['코드']).zfill(6)], sales_class='매도')     
+                                    data['구매수량'],
+                                    round(data['종가'] * data['구매수량'] * 0.00140527, 0),  
+                                    '매수']
+                            self.write_jounal(inform=inform)    
+                            self.MONEY -= data['종가'] * data['구매수량'] + round(data['종가'] * data['구매수량'] * 0.00140527, 0)
+                            self.upsert_asset(code=str(data['코드']).zfill(6), amount=data['구매수량'], sales_class='매수', price=data['종가'])
+                            self.test[day] = deepcopy(self.ASSET)         
             except:
                 pass
+            
 class Utils:
     def __init__(self):
         pass
